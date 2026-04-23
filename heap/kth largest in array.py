66220f6ce7af67bebd16a884fa3b93eaf520d62f
Umanshiii <umanshi121@gmail.com>
2026-04-23 23:45:43 +0530

    heap

---
+++ heap/kth largest in array.py
@@ -0,0 +1,16 @@
+'''Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.'''
+
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+
+        for num in nums:
+            heapq.heappush(heap, num)
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return heap[0]
